71a86f5ce409bd322360279ca411a8df471e3bd0
anlas17 <49290462+anlas17@users.noreply.github.com>
2019-06-01 02:33:36 +0200

    DB + display
    DB ready, display ready, sound ready

--- Soundboard/app/src/main/java/com/example/soundboard/MainActivity.java
+++ Soundboard/app/src/main/java/com/example/soundboard/MainActivity.java
@@ -1,36 +1,25 @@
 package com.example.soundboard;
 
 import android.media.MediaPlayer;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    //creating mediaplayers
-    MediaPlayer bow;
-    MediaPlayer fart;
-    MediaPlayer mp5;
-    MediaPlayer sweep;
-    //crasher appen
-    /*
-    final TextView bowText = findViewById(R.id.textView);
-    final TextView fartText = findViewById(R.id.textView2);
-    final TextView mp5Text = findViewById(R.id.textView4);
-    final TextView sweepText = findViewById(R.id.textView3);
-    */
+    
+    private MediaPlayer bow;
+    private MediaPlayer fart;
+    private MediaPlayer mp5;
+    private MediaPlayer sweep;
 
     Integer bowAmount = new Integer(0);
     Integer sweepAmount = new Integer(0);
     Integer fartAmount = new Integer(0);
     Integer mp5Amount = new Integer(0);
-
-    Sound fartS = new Sound("fart", 0);
-    Sound mp5S = new Sound("mp5", 0);
-    Sound sweepS = new Sound("sweep", 0);
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,37 +28,36 @@ public class MainActivity extends AppCompatActivity {
 
         SoundRepository sdb = new SoundRepository(getApplicationContext());
 
-        //creating sounds from files
-        bow = MediaPlayer.create(this, R.raw.bow);
-        fart = MediaPlayer.create(this, R.raw.fart);
-        mp5 = MediaPlayer.create(this, R.raw.mp5);
-        sweep = MediaPlayer.create(this, R.raw.sweep);
+        TextView bowText = (TextView) findViewById(R.id.textbow);
+        TextView fartText = (TextView) findViewById(R.id.textfart);
+        TextView mp5Text = (TextView) findViewById(R.id.textmp5);
+        TextView sweepText = (TextView) findViewById(R.id.textsweep);
+
+        bowText.setText(String.valueOf(bowAmount));
+        fartText.setText(String.valueOf(fartAmount));
+        mp5Text.setText(String.valueOf(mp5Amount));
+        sweepText.setText(String.valueOf(sweepAmount));
 
 
-        //set text to 0 placeholder
-        /*
-        bowText.setText(bowAmount);
-        fartText.setText(fartAmount);
-        mp5Text.setText(mp5Amount);
-        sweepText.setText(sweepAmount);
-        */
     }
-
 
     //start sound with button
     public void bow(View view){
         SoundRepository sdb = new SoundRepository(getApplicationContext());
+        TextView bowText = (TextView) findViewById(R.id.textbow);
+        bow = MediaPlayer.create(this, R.raw.bow);
+
         bow.start();
-        Sound bowS = new Sound("bow",0);
+        Sound bowS;
 
         try {
             bowS = sdb.getSound("bow");
             bowAmount = bowS.getAmount() + 1;
-            //set text bowamount
+            bowText.setText(String.valueOf(bowAmount));
             bowS.setAmount(bowAmount);
             sdb.updateSound(bowS.getName(), bowS.getAmount());
         }catch(Exception e){
-            //set text bowamount to 0
+            bowText.setText(String.valueOf(bowAmount));
             sdb.insertSound("bow",0);
         }
 
@@ -77,51 +65,60 @@ public class MainActivity extends AppCompatActivity {
     //start sound with button
     public void fart(View view){
         SoundRepository sdb = new SoundRepository(getApplicationContext());
+        TextView fartText = (TextView) findViewById(R.id.textfart);
+        fart = MediaPlayer.create(this, R.raw.fart);
+
         fart.start();
-        Sound fartS = new Sound("fart",0);
+        Sound fartS;
 
         try {
             fartS = sdb.getSound("fart");
             fartAmount = fartS.getAmount() + 1;
-            //set text bowamount
+            fartText.setText(String.valueOf(fartAmount));
             fartS.setAmount(fartAmount);
             sdb.updateSound(fartS.getName(), fartS.getAmount());
         }catch(Exception e){
-            //set text fartamount to 0
+            fartText.setText(String.valueOf(fartAmount));
             sdb.insertSound("fart",0);
         }
     }
     //start sound with button
     public void mp5(View view){
         SoundRepository sdb = new SoundRepository(getApplicationContext());
+        TextView mp5Text = (TextView) findViewById(R.id.textmp5);
+        mp5 = MediaPlayer.create(this, R.raw.mp5);
+
         mp5.start();
-        Sound mp5S = new Sound("mp5",0);
+        Sound mp5S;
 
         try {
             mp5S = sdb.getSound("mp5");
             mp5Amount = mp5S.getAmount() + 1;
-            //set text mp5amount
+            mp5Text.setText(String.valueOf(mp5Amount));
             mp5S.setAmount(mp5Amount);
             sdb.updateSound(mp5S.getName(), mp5S.getAmount());
         }catch(Exception e){
-            //set text mp5amount to 0
+            mp5Text.setText(String.valueOf(mp5Amount));
             sdb.insertSound("mp5",0);
         }
     }
     //start sound with button
     public void sweep(View view){
         SoundRepository sdb = new SoundRepository(getApplicationContext());
+        TextView sweepText = (TextView) findViewById(R.id.textsweep);
+        sweep = MediaPlayer.create(this, R.raw.sweep);
+
         sweep.start();
-        Sound sweepS = new Sound("sweep",0);
+        Sound sweepS;
 
         try {
             sweepS = sdb.getSound("sweep");
             sweepAmount = sweepS.getAmount() + 1;
-            //set text sweepamount
+            sweepText.setText(String.valueOf(sweepAmount));
             sweepS.setAmount(sweepAmount);
             sdb.updateSound(sweepS.getName(), sweepS.getAmount());
         }catch(Exception e){
-            //set text sweepamount to 0
+            sweepText.setText(String.valueOf(sweepAmount));
             sdb.insertSound("sweep",0);
         }
     }
